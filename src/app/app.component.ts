1813fe02ece080e0deb246d7716a6fa6277f33da
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-09-21 13:14:58 +0200

    Add wallet-selector.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "near-wallet-selector-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'guest-book-angular';
+  showImport: boolean;
 }
